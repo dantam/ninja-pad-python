7791b775b6e73d639df21a874da3d102cf43de5d
Dan Tam <dantam@gmail.com>
2020-03-27 07:24:35 -0700

    [ninja][datastore] config and test

--- src/tests/test_db_config.py
+++ src/tests/test_db_config.py
@@ -43,7 +43,7 @@ db_config = {
         DC.DATABASE_NAME: TEST_DATABASE_NAME_2,
         DC.DATABASE_USER: TEST_DATABASE_USER,
     }],
-    DC.PRIVACY_ENFORCER_STORE: [{
+    DC.PRIVACY_ENFORCER_LOGS: [{
         DC.DATABASE_ENGINE: DE.SQLITE,
         DC.DATABASE_NAME: TEST_DATABASE_NAME_1,
         DC.DATABASE_USER: TEST_DATABASE_USER,
@@ -68,7 +68,7 @@ class TestDatastoreConfig(unittest.TestCase):
             (DC.LOCATION_LOGS, dc.get_location_logs_config),
             (DC.NOTIFICATION_LOGS, dc.get_notification_logs_config),
             (DC.CONTAMINATION_LOGS, dc.get_contamination_logs_config),
-            (DC.PRIVACY_ENFORCER_STORE, dc.get_privacy_enforcer_store_config),
+            (DC.PRIVACY_ENFORCER_LOGS, dc.get_privacy_enforcer_logs_config),
             (DC.ON_DEVICE_STORE, dc.get_on_device_store_config),
         ]
         for test_pair in test_pairs:
